f49d28b49669e7d89cbade109cc756bd5af2d471
gy_lyons <gy_lyons@163.com>
2019-02-01 18:00:13 +0800

    测试 UIView 快速调整的方法

--- SwiftDemo/Controllers/Home/GYHomeViewController.swift
+++ SwiftDemo/Controllers/Home/GYHomeViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class GYHomeViewController: GYRootViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         //不想在 tabbarItem 上显示 title 就使用 navigationItem 的方法
         self.navigationItem.title = "首页"
-
+        
+        self.label.gy_top = 150
+        self.label.gy_left = 50
+        self.label.gy_bottom = 150
+        self.label.gy_right = 50
     }
 
 }

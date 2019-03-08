78e17bf3e41814e2740464c954cf7950b3f004b5
gy_lyons <gy_lyons@163.com>
2019-03-08 16:30:33 +0800

    demo列表

--- SwiftDemo/Controllers/Home/GYHomeViewController.swift
+++ SwiftDemo/Controllers/Home/GYHomeViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import Kingfisher
+import Accelerate
 
 class GYHomeViewController: GYRootViewController {
 
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    struct item {
+        var title: String
+        var className: String
+    }
+    let cellID = "cell"
+    var list: Array<item> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //不想在 tabbarItem 上显示 title 就使用 navigationItem 的方法
         self.navigationItem.title = "首页"
         
-        self.label.gy_top = 150
-        self.label.gy_left = 50
-        self.label.gy_bottom = 150
-        self.label.gy_right = 50
+        self.list = [item(title: "高斯模糊", className: "GYGaussianBlurViewController"),
+                     item(title: "渐变导航", className: "GYGaussianBlurViewController")]
+        
+        // 2.设置数据源代理
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        // 4.注册cell
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
     }
+}
 
+extension GYHomeViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID)
+        cell?.textLabel?.text = self.list[indexPath.row].title
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let className = self.list[indexPath.row].className
+        self.navigationController?.pushViewController(className.stringChangeToVC()!, animated: true)
+        print("点击了\(indexPath.row)")
+    }
+    
 }

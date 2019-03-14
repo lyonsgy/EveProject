f8ffdfc5ff8076606a7d261cf65dc0119f3c4dcc
gy_lyons <gy_lyons@163.com>
2019-03-14 16:58:05 +0800

    新增国际化配置工具 languageHelper

--- SwiftDemo/Helper/GYLanguageHelper/GYLanguageHelper.swift
+++ SwiftDemo/Helper/GYLanguageHelper/GYLanguageHelper.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
+let GYLanguageKey = "GYLanguageKey"
+
 enum language:String {
-    case en = "en-US"
-    case zh = "zh-Hans-US"
-    case ja = "ja-US"
+    case system = ""
+    case en = "en"
+    case zh = "zh-Hans"
+    case zhHK = "zh-HK"
+    case ja = "ja"
 }
 
 ///  languageType 语言协议
@@ -29,27 +33,74 @@ extension language: languageType {
             return "日本語"
         case .zh:
             return "简体中文"
+        case .zhHK:
+            return "繁体中文（香港）"
+        default:
+            return "系统语言"
         }
     }
+    
 }
 class GYLanguageHelper {
-    /// 当前语言
-    var nowlanguage: String {
-        get {
-            return language(rawValue: GYLanguageHelper.getLanguage())?.title ?? ""
+    static func setUserLanguage (userLanguage:String){
+        //跟随手机系统
+        guard userLanguage.count>0 else {
+            resetSystemLanguage()
+            return
         }
+        UserDefaults.standard.set(userLanguage, forKey: GYLanguageKey)
+        UserDefaults.standard.set([userLanguage], forKey: "AppleLanguages")
+        UserDefaults.standard.synchronize()
     }
-    static func getLanguage() -> String {
-        let languages:Array = UserDefaults.standard.value(forKey: "AppleLanguages") as! Array<String>
-        let currentLanguage = languages.first
-        return currentLanguage ?? ""
+    static func initSystemLanguage() {
+        let userLanguage = UserDefaults.standard.value(forKey: GYLanguageKey)
+        guard userLanguage == nil else {
+            UserDefaults.standard.set([(userLanguage as! String)], forKey: "AppleLanguages")
+            UserDefaults.standard.synchronize()
+            return
+        }
+        UserDefaults.standard.set(nil, forKey: "AppleLanguages")
+        UserDefaults.standard.synchronize()
     }
-    static func changeLanguage (language:String){
-        let lans = [language]
-        UserDefaults.standard.set(lans, forKey: "AppleLanguages")
+    static func resetSystemLanguage() {
+        UserDefaults.standard.removeObject(forKey: GYLanguageKey)
+        UserDefaults.standard.set(nil, forKey: "AppleLanguages")
+        UserDefaults.standard.synchronize()
+    }
+    static func currentLanName() -> String {
+        if let userLanguage = UserDefaults.standard.value(forKey: GYLanguageKey) {
+            return language(rawValue: (userLanguage as! String))!.title
+        }
+        return "系统语言"
+    }
+    static func currentLanguage() -> String? {
+        let userLanguage = UserDefaults.standard.value(forKey: GYLanguageKey)
+        guard userLanguage == nil else {
+            return (userLanguage as! String)
+        }
+        return Locale.preferredLanguages.first
     }
 }
 
-
+extension String {
+    //返回第一次出现的指定子字符串在此字符串中的索引
+    //（如果backwards参数设置为true，则返回最后出现的位置）
+    func positionOf(sub:String, backwards:Bool = false)->Int {
+        var pos = -1
+        if let range = range(of:sub, options: backwards ? .backwards : .literal ) {
+            if !range.isEmpty {
+                pos = self.distance(from:startIndex, to:range.lowerBound)
+            }
+        }
+        return pos
+    }
+    func gySubString(to index: Int) -> String {
+        return String(self[..<self.index(self.startIndex, offsetBy: index)])
+    }
+    
+    func gySubString(from index: Int) -> String {
+        return String(self[self.index(self.startIndex, offsetBy: index)...])
+    }
+}
 
 

0d51242832ad6a23b03fd5df46286d21f3b3da84
gy_lyons <gy_lyons@163.com>
2019-07-24 10:16:05 +0800

    增加和h5页面的交互

--- SwiftDemo/Root/GYRootNavigationController.swift
+++ SwiftDemo/Root/GYRootNavigationController.swift
@@ -8,8 +8,36 @@
 
 import UIKit
 
-class GYRootNavigationController: UINavigationController {
-    
+class GYRootNavigationController: UINavigationController, UINavigationBarDelegate {
+    // 实现navigationBar(_: shouldPop:)
+    func navigationBar(_ navigationBar: UINavigationBar, shouldPop item: UINavigationItem) -> Bool {
+        var shouldPop = true
+        
+        // 已修改（标记1）
+        let viewControllersCount = self.viewControllers.count
+        let navigationItemsCount = navigationBar.items?.count
+        
+        if(viewControllersCount < navigationItemsCount!){
+            return shouldPop
+        }
+        if let topViewController: UIViewController = self.topViewController {
+            if(topViewController is NavigationControllerBackButtonDelegate){
+                let delegate = topViewController as! NavigationControllerBackButtonDelegate
+                shouldPop = delegate.shouldPopOnBackButtonPress()
+            }
+        }
+        if(shouldPop == false){
+            isNavigationBarHidden = true
+            isNavigationBarHidden = false
+        }else{
+            if(viewControllersCount >= navigationItemsCount!){
+                DispatchQueue.main.async { () -> Void in
+                    self.popViewController(animated: true)
+                }
+            }
+        }
+        return shouldPop
+    }
 }
 
 extension GYRootNavigationController
@@ -22,4 +50,9 @@ extension GYRootNavigationController
         }
         super.pushViewController(viewController, animated: animated)
     }
+}
+
+// 定义一个protocol，实现它的类，自定义pop规则、逻辑或方法
+protocol NavigationControllerBackButtonDelegate {
+    func shouldPopOnBackButtonPress() -> Bool
 }
